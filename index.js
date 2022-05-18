72d06a4842623d3e881b4f0467dd7a043e68daba
mosheurpau <mosheur.pau@gamil.com>
2022-05-19 03:02:00 +0600

    create api for user added new task

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c0tgt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("TodoTask").collection("task");
+
+    app.get("/task", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+
+    // POST
+    app.post("/task", async (req, res) => {
+      const newTask = req.body;
+      const result = await serviceCollection.insertOne(newTask);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("To-Do server");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
